09be4f2b464ce7f3f2e459e7b0bde146b47ae988
DarianAli <darianaliapr@gmail.com>
2026-02-20 15:05:01 +0700

    feat(class): add class update API

--- src/Router/classRouter.ts
+++ src/Router/classRouter.ts
@@ -1,10 +1,11 @@
 import express from "express"
-import { addData } from "../middleware/classValidation"
-import { createClass } from "../controller/class-controller"
+import { addData, updateData } from "../middleware/classValidation"
+import { classUpdate, createClass } from "../controller/class-controller"
 
 const app = express()
 app.use(express.json())
 
 app.post("/create", [addData], createClass)
+app.put("/update", [updateData], classUpdate)
 
 export default app
--- src/controller/class-controller.ts
+++ src/controller/class-controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaClient, status } from "../../generated/prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { v4 as uuidv4 } from "uuid"
 import "dotenv/config";
@@ -33,6 +33,56 @@ export const createClass = async (request: Request, response: Response) => {
         response.status(500).json({
             status: false,
             message: `Failed to create a class.`
+        })
+        return
+    }
+}
+
+export const classUpdate = async (request: Request, response: Response) => {
+    try {
+        const { idClass } = request.params;
+        const { class_name, class_program } = request.body;
+
+        if (!idClass) {
+            response.status(400).json({
+                status: false,
+                message: `idClas is required.`
+            })
+            return
+        }
+
+        const findClass = await prisma.classes.findFirst({
+            where: { idClass: Number(idClass) }
+        })
+
+        if (!findClass) {
+            response.status(404).json({
+                status: false,
+                message: `Class did not found.`
+            })
+            return
+        }
+        
+        const updateData = await prisma.classes.update({
+            data: {
+                class_name: class_name || findClass.class_name,
+                class_program: class_program || findClass.class_program
+            },
+            where: { idClass: Number(idClass) }
+        })
+
+        response.status(200).json({
+            status: true,
+            data: updateData,
+            message: `Successfully update data.`
+        })
+        return
+    } catch (error) {
+        console.error(error)
+
+        response.status(500).json({
+            status: false,
+            message: `Internal server error.`
         })
         return
     }
