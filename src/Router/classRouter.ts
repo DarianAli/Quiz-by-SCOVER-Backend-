14e1ff432be4f8b05e56945745257052404f9432
DarianAli <darianaliapr@gmail.com>
2026-02-21 13:46:40 +0700

    feat(class): add get class API

--- src/Router/classRouter.ts
+++ src/Router/classRouter.ts
@@ -1,11 +1,13 @@
 import express from "express"
 import { addData, updateData } from "../middleware/classValidation"
-import { classUpdate, createClass } from "../controller/class-controller"
+import { classUpdate, createClass, getAllData, getById } from "../controller/class-controller"
 
 const app = express()
 app.use(express.json())
 
 app.post("/create", [addData], createClass)
 app.put("/update/:idClass", [updateData], classUpdate)
+app.get("/allData", getAllData)
+app.get("/get/:idClass", getById)
 
 export default app
