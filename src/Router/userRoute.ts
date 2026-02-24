81bbdeea568e311924ce399b6c9451f63db45b8e
DarianAli <darianaliapr@gmail.com>
2026-02-24 15:52:59 +0700

    feat(user): add PUT API new update user data API

--- src/Router/userRoute.ts
+++ src/Router/userRoute.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { addData } from "../middleware/userValidation";
+import { addData, updateData } from "../middleware/userValidation";
 import { phoneValidation } from "../validator/phoneValidator";
-import { createUser, getAllUser, getById, registerLimiter } from "../controller/user-controller";
+import { createUser, getAllUser, getById, updateUser } from "../controller/user-controller";
+import { registerLimiter, updateLimiter } from "../middleware/rateLimiter";
 
 const app = express()
 app.use(express.json())
@@ -9,5 +10,6 @@ app.use(express.json())
 app.post("/register", registerLimiter, [addData, phoneValidation(["phone_number", "parent_phone_number"])], createUser)
 app.get("/getAll", getAllUser)
 app.get("/get/:idUser", getById)
+app.put("/update/:idUser", updateLimiter, [updateData, phoneValidation(["phone_number", "parent_phone_number"])], updateUser)
 
 export default app
