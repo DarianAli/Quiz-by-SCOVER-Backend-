a1ada5bf028165bdc6dc3df75f4af1709fa844c4
Ardavand <ArDavandRD@gmail.com>
2026-02-25 11:48:51 +0700

    fix

---
+++ src/controller/quiz-controller.ts
@@ -0,0 +1,104 @@
+import { Response, Request } from "express"
+import { PrismaClient } from "@prisma/client"
+import { request } from "http";
+
+const prisma = new PrismaClient ({ errorFormat: "pretty"})
+
+export const getAllQuiz = async (request: Request, response: Response) => {
+    try {
+        const { search } = request.query;
+        
+        const AllQuiz = await prisma.quiz.findMany({
+            where: {quiz_title: {contains: search?.toString() || ""}}, 
+            include: {
+                subject: true,
+                questions: true,
+                scores: true
+            }  
+        })
+
+        return response.status(200).json({
+            success: true,
+            data : AllQuiz,
+            message : "All quiz found successfully"
+        })
+    } catch (error) {
+        return response.status(400).json({
+            success: false,
+            message: `failed to fetch all quiz. ${error}`
+        })
+    }
+};
+
+export const getQuizById = async (request: Request, response: Response) => {
+    try {
+        const idQuiz = request.params.idQuiz;
+
+        if (!idQuiz) {
+            return response.status(400).json({
+                success: false,
+                message: "id Quiz is required"
+            })
+        }
+
+        const AllQuiz = await prisma.quiz.findUnique({
+            where: { idQuiz: Number(idQuiz)},
+            include: {
+                subject: true,
+                questions: true,
+                scores: true
+            }
+
+        })
+
+        if (!AllQuiz) {
+            return response.status(404).json({
+                success: false,
+                message: "quiz not found"
+            })
+        }
+
+        return response.status(200).json({
+            success: true,
+            data : AllQuiz,
+            message : "quiz found successfully"
+        })
+
+    } catch (error) {
+        return response.status(400).json({
+            success: false,
+            message: `failed to fetch quiz. ${error}`
+        })
+    }
+}
+
+export const createQuiz = async (request: Request, response: Response) => {
+    
+}
+
+export const updateQuiz = async (request: Request, response: Response) => {
+    
+}
+
+
+export const deleteQuiz = async (request: Request, response: Response) => {
+    try {
+      const { idQuiz } = request.params;
+  
+      const deletedQuiz = await prisma.quiz.delete({
+        where: { idQuiz: Number(idQuiz) }
+      });
+  
+      return response.status(200).json({
+        status: true,
+        data: deletedQuiz,
+        message: "Quiz deleted successfully"
+      })
+  
+    } catch (error) {
+      return response.status(500).json({
+        status: false,
+        message: `Failed to delete quiz. ${error}`,
+      })
+    }
+}
