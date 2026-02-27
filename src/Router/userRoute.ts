99bd7aa87cb767cb5b3f7e9c03ff409bc5f5b602
DarianAli <darianaliapr@gmail.com>
2026-02-27 14:37:41 +0700

    feat(user): add authentication of user, login, verify token and verify role

--- src/Router/userRoute.ts
+++ src/Router/userRoute.ts
@@ -1,18 +1,21 @@
 import express from "express";
-import { addData, updateData, updatePasswordData, updatePasswordDataUser } from "../middleware/userValidation";
+import { addData, updateData, updatePasswordData, updatePasswordDataUser, verifyLogin } from "../middleware/userValidation";
 import { phoneValidation } from "../validator/phoneValidator";
-import { createUser, deleteUser, getAllUser, getById, updatePasswordAdmin, updatePasswordUser, updateUser } from "../controller/user-controller";
+import { auth, createUser, deleteUser, getAllUser, getById, updatePasswordAdmin, updatePasswordUser, updateUser } from "../controller/user-controller";
 import { deleteLimiter, passwordLimiter, registerLimiter, updateLimiter } from "../middleware/rateLimiter";
+import { verifyRole, verifyToken } from "../middleware/auth";
+import { onlyAdminCanChangeRole } from "../middleware/onlyAdminUpdate";
 
 const app = express()
 app.use(express.json())
 
-app.post("/register", registerLimiter, [addData, phoneValidation(["phone_number", "parent_phone_number"])], createUser)
-app.get("/getAll", getAllUser)
-app.get("/get/:idUser", getById)
-app.put("/update/:idUser", updateLimiter, [updateData, phoneValidation(["phone_number", "parent_phone_number"])], updateUser)
-app.put("/:idUser/admin/password", passwordLimiter, [updatePasswordData], updatePasswordAdmin)
-app.put("/password/:idUser", passwordLimiter, [updatePasswordDataUser], updatePasswordUser)
-app.delete("/delete/:idUser",deleteLimiter, deleteUser)
+app.post("/register", registerLimiter, [verifyToken,  verifyRole(["ADMIN"]), addData, phoneValidation(["phone_number", "parent_phone_number"])], createUser)
+app.post("/login", [verifyLogin], auth)
+app.get("/getAll", [verifyToken, verifyRole(["ADMIN"])], getAllUser)
+app.get("/get/:idUser", [verifyToken, verifyRole(["ADMIN", "TENTOR", "STUDENT"])], getById)
+app.put("/update/:idUser", updateLimiter, [verifyToken, verifyRole(["ADMIN", "TENTOR", "STUDENT"]), onlyAdminCanChangeRole, updateData, phoneValidation(["phone_number", "parent_phone_number"])], updateUser)
+app.put("/:idUser/admin/password", passwordLimiter, [verifyToken, verifyRole(["ADMIN"]), updatePasswordData], updatePasswordAdmin)
+app.put("/password/:idUser", passwordLimiter, [verifyToken, verifyRole(["ADMIN", "TENTOR", "STUDENT"]), updatePasswordDataUser], updatePasswordUser)
+app.delete("/delete/:idUser",deleteLimiter, [verifyToken, verifyRole(["ADMIN"])], deleteUser)
 
 export default app
