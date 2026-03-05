f7cbcb94d7cdd06b174624f1d5af50cc71aeae91
DarianAli <darianaliapr@gmail.com>
2026-03-05 14:02:32 +0700

    feat(subject): add get all data APi and add a existing validation

--- src/Router/subjectRoute.ts
+++ src/Router/subjectRoute.ts
@@ -1,13 +1,14 @@
 import express from "express"
 import { verifyRole, verifyToken } from "../middleware/auth"
 import { createDataValidation } from "../middleware/subjectValidation"
-import { createSubject } from "../controller/subject-controller"
+import { createSubject, getAllSubject } from "../controller/subject-controller"
 import { postLimiter } from "../middleware/rateLimiter"
 
 const app = express()
 app.use(express.json())
 
 app.post("/create", postLimiter, [verifyToken, verifyRole(["ADMIN"]), createDataValidation], createSubject)
+app.get("/all-data", [verifyToken, verifyRole(["ADMIN"])], getAllSubject)
 
 
 export default app
--- src/controller/subject-controller.ts
+++ src/controller/subject-controller.ts
@@ -10,6 +10,18 @@ export const createSubject = async (request: Request, response: Response) => {
         const { subject_name } = request.body;
         const uuid = uuidv4()
 
+        const existing = await prisma.subject.findFirst({
+            where: { subject_name: subject_name }
+        })
+
+        if (existing) {
+            response.status(400).json({
+                status: false,
+                message: `Subject name already exists`
+            })
+            return
+        }
+
         const createData = await prisma.subject.create({
             data: {
                 uuid,
@@ -21,6 +33,40 @@ export const createSubject = async (request: Request, response: Response) => {
             status: true,
             data: createData,
             message: `Successfully created a subject`
+        })
+        return
+    } catch (error) {
+        console.error(error)
+
+        response.status(500).json({
+            status: false,
+            message: `Internal server error.`
+        })
+        return
+    }
+}
+
+export const getAllSubject = async (request: Request, response: Response) => {
+    try {
+        const  search  = request.query.search?.toString() ?? "";
+
+
+        const findSubject = await prisma.subject.findMany({
+            where: { subject_name: { contains: search?.toString() } }
+        })
+
+        if (findSubject.length === 0) {
+            response.status(404).json({
+                status: false,
+                message: `Subject not found.`
+            })
+            return
+        }
+
+        response.status(200).json({
+            status: true,
+            data: findSubject,
+            message: `Showing all data.`
         })
         return
     } catch (error) {
