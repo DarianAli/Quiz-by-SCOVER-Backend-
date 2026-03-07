ce9329d30de7dff9dba47f6da3b8985c10bab2c7
DarianAli <darianaliapr@gmail.com>
2026-03-07 12:14:27 +0700

    feat(subject): add get all user by user or class

--- src/Router/subjectRoute.ts
+++ src/Router/subjectRoute.ts
@@ -1,7 +1,7 @@
 import express from "express"
 import { verifyRole, verifyToken } from "../middleware/auth"
 import { createDataValidation, updateDataValidation } from "../middleware/subjectValidation"
-import { createSubject, deleteSubject, getAllSubject, getByID, updateSubject } from "../controller/subject-controller"
+import { createSubject, deleteSubject, getAllSubject, getByID, getSubjectByUser, updateSubject } from "../controller/subject-controller"
 import { deleteLimiter, postLimiter, updateLimiter } from "../middleware/rateLimiter"
 
 const app = express()
@@ -10,6 +10,7 @@ app.use(express.json())
 app.post("/create", postLimiter, [verifyToken, verifyRole(["ADMIN"]), createDataValidation], createSubject)
 app.get("/all-data", [verifyToken, verifyRole(["ADMIN"])], getAllSubject)
 app.get("/get/:idSubject", [verifyToken, verifyRole(["ADMIN"])], getByID)
+app.get("/:idUser/subjects", [verifyToken, verifyRole(["ADMIN", "TENTOR", "STUDENT"])], getSubjectByUser)
 app.put("/update-data/:idSubject", updateLimiter, [verifyToken, verifyRole(["ADMIN"]), updateDataValidation], updateSubject)
 app.delete("/delete-subject/:idSubject", deleteLimiter, [verifyToken, verifyRole(["ADMIN"]), deleteSubject])
 
