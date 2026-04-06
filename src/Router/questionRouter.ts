a93b328da2f0e0711a28629230ae1d2def7b25da
Ardavand <ArDavandRD@gmail.com>
2026-04-06 15:08:28 +0700

    fix(option): add upload feature for option

--- src/Router/questionRouter.ts
+++ src/Router/questionRouter.ts
@@ -3,12 +3,13 @@ import { createQuestion, updateQuestion, getAllQuestion, getQuestionById, delete
 import { verifyRole, verifyToken } from "../middleware/auth"
 import { postLimiter, updateLimiter, deleteLimiter } from "../middleware/rateLimiter"
 import { verifyAddQuestion, verifyEditQuestion } from "../middleware/questionValidation"
+import uploadQuestionFile from "../middleware/questionUpload"
 
 const app = express()
 app.use(express.json({ strict: false }))
 
-app.post('/add', postLimiter, [verifyToken, verifyRole(["ADMIN", "TENTOR"]), verifyAddQuestion], createQuestion)
-app.put('/update/:idQuestion', updateLimiter, [verifyToken, verifyRole(["ADMIN", "TENTOR"]), verifyEditQuestion], updateQuestion)
+app.post('/add', postLimiter, [verifyToken, verifyRole(["ADMIN", "TENTOR"]), uploadQuestionFile.single("file"), verifyAddQuestion], createQuestion)
+app.put('/update/:idQuestion', updateLimiter, [verifyToken, verifyRole(["ADMIN", "TENTOR"]), uploadQuestionFile.single("file"), verifyEditQuestion], updateQuestion)
 app.get('/allData', [verifyToken, verifyRole(["ADMIN", "TENTOR"])], getAllQuestion)
 app.get('/byID/:idQuestion', [verifyToken, verifyRole(["ADMIN", "TENTOR"])], getQuestionById)
 app.delete('/delete/:idQuestion', deleteLimiter, [verifyToken, verifyRole(["ADMIN"])], deleteQuestion)
