027c729548369b217d3319b21827b7dc061c20b8
Ardavand <ArDavandRD@gmail.com>
2026-04-06 15:59:51 +0700

    fix(user): add option feature and fixing logic in user upload

--- src/Router/userRoute.ts
+++ src/Router/userRoute.ts
@@ -1,13 +1,14 @@
 import express, { Request, Response, NextFunction } from "express";
-import { addData, updateData, updatePasswordData, updatePasswordDataUser, verifyLogin } from "../middleware/userValidation";
+import { addData, profileData, updateData, updatePasswordData, updatePasswordDataUser, verifyLogin } from "../middleware/userValidation";
 import { phoneValidation } from "../validator/phoneValidator";
-import { auth, createUser, deleteUser, getAllUser, getById, updatePasswordAdmin, updatePasswordUser, updateUser } from "../controller/user-controller";
+import { auth, createUser, deleteUser, getAllUser, getById, updatePasswordAdmin, updatePasswordUser, updatePicture, updateUser } from "../controller/user-controller";
 import { deleteLimiter, passwordLimiter, postLimiter, registerLimiter, updateLimiter } from "../middleware/rateLimiter";
 import { verifyRole, verifyToken } from "../middleware/auth";
 import { onlyAdminCanChangeRole } from "../middleware/onlyAdminUpdate";
 import { verifyOwnershipOrAdmin } from "../middleware/validation";
 import { bulkCreateUsers } from "../controller/user-controller";
 import { uploadExcel } from "../middleware/uploadMidleware";
+import uploadUserFile from "../middleware/userUpload";
 
 const app = express()
 app.use(express.json())
@@ -33,6 +34,7 @@ app.get("/get/:idUser", [verifyToken, verifyRole(["ADMIN", "TENTOR", "STUDENT"])
 app.put("/update/:idUser", updateLimiter, [verifyToken, verifyRole(["ADMIN", "TENTOR", "STUDENT"]), verifyOwnershipOrAdmin, onlyAdminCanChangeRole, updateData, phoneValidation(["phone_number", "parent_phone_number"])], updateUser)
 app.put("/:idUser/admin/password", passwordLimiter, [verifyToken, verifyRole(["ADMIN"]), updatePasswordData], updatePasswordAdmin)
 app.put("/password/:idUser", passwordLimiter, [verifyToken, verifyRole(["ADMIN", "TENTOR", "STUDENT"]), verifyOwnershipOrAdmin, updatePasswordDataUser], updatePasswordUser)
+app.put("/profile/:idUser", updateLimiter, [verifyToken, verifyRole(["ADMIN", "TENTOR", "STUDENT"]), verifyOwnershipOrAdmin, uploadUserFile.single("photoProfile"), profileData], updatePicture)
 app.delete("/delete/:idUser",deleteLimiter, [verifyToken, verifyRole(["ADMIN"])], deleteUser)
 
 export default app
